016eb942640b92a8a682b73bfd613ec0fe5dfad0
stropee <stropee@ampus-eni.fr>
2024-10-08 16:15:43 +0200

    module 2 - composants

---
+++ src/app/components/article/article.component.ts
@@ -0,0 +1,51 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      article works!
+    </p>
+  `,
+  styleUrl: './article.component.scss'
+})
+export class ArticleComponent {
+
+  public test: string = "HEllo !"
+
+
+  ngOnChanges(): void {
+    console.log(1)
+  }
+
+  ngOnInit() {
+    console.log(2)
+  }
+
+  ngDoCheck() {
+    console.log(3)
+  }
+
+  ngAfterContentInit() {
+    console.log(4)
+  }
+
+  ngAfterContentChecked() {
+    console.log(5)
+  }
+
+  ngAfterViewInit() {
+    console.log(6)
+  }
+
+  ngAfterViewChecked() {
+    console.log(7)
+  }
+
+  ngOnDestroy() {
+    console.log(8)
+  }
+
+}
